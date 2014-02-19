7f78d9101bb9a119d727de6027e459313c4d0cb1
withthelemons <with.the.lemons@gmail.com>
2014-02-19 18:33:12 -0500

    updated version to 1.0.2

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Satoshi");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "-beta"
+#define CLIENT_VERSION_SUFFIX   ""
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
@@ -36,7 +36,7 @@ const std::string CLIENT_NAME("Satoshi");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "88e2a2e"
+#    define GIT_COMMIT_ID ""
 #    define GIT_COMMIT_DATE "Thu, 5 Dec 2013 22:52:50 -1000"
 #endif
 
